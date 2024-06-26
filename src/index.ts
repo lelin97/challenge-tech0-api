288beb580a81fbe66df795439f2321ef873e4c26
Lelo Monteiro <eliezermonteiro97@outlook.com>
2024-06-26 10:53:58 -0300

    feat: add cors and uuid

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import router from "./routes/routes";
+import cors from "cors";
 
 const app = express();
 const port = 4050;
 
 AppDataSource.initialize().then(async () => {
   console.log("Conexão typeorm-postgres inicializada com sucesso.");
-
-  app.use(express.json());
-
-  // app.get("/", (req, res) => {
-  //   return res.json("Funcionando");
-  // });
-
-  app.use(router);
-
-  app.listen(port);
 });
 
-//   .catch((error) => {
-//     console.log(
-//       "Erro durante a inicialização da conexão typeorm-postres.",
-//       error
-//     );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Content-Type", "*/*");
+  cors({
+    credentials: true,
+    origin: true,
+    exposedHeaders: "*",
+    preflightContinue: false,
+    methods: ["GET", "PUT", "POST", "DELETE"],
+  });
+  next();
+});
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(port);
